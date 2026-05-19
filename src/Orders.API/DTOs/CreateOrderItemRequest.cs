eada07b4a49f75d04d8bc03c4460147460681682
dainunez <daigianela04@gmail.com>
2026-05-19 01:18:38 -0300

    Orders (Models, DTOs, Exception, ExceptionHandler)

---
+++ src/Orders.API/DTOs/CreateOrderItemRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Orders.API.DTOs
+{
+    public class CreateOrderItemRequest
+    {
+        public Guid ProductoId { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
---
+++ src/Orders.API/DTOs/UpdateOrderStatusResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Orders.API.DTOs
+{
+    public class UpdateOrderStatusResponse
+    {
+        public Guid Id { get; set; }
+        public string Estado { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+    }
+}
