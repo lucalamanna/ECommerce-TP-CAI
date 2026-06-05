3a74be654b601ca113cf260b9c43f4b29c598d33
dainunez <daigianela04@gmail.com>
2026-06-05 19:58:28 -0300

    Notifications (Endpoint POST + Extensions)

---
+++ src/Notifications.API/Extensions/EndpointExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Notifications.API.Controllers;
+
+namespace Notifications.API.Extensions
+{
+    public static class EndpointExtensions
+    {
+        public static void MapAppEndpoints(this WebApplication app)
+        {
+            app.MapNotificationsEndpoints();
+        }
+    }
+}
---
+++ src/Notifications.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Notifications.API.Data;
+using Notifications.API.Services;
+using System.Reflection;
+
+namespace Notifications.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddAppServices(this IServiceCollection services)
+        {
+            services.AddSingleton<DatabaseInitializer>();
+            services.AddScoped<NotificationRepository>();
+            services.AddScoped<NotificationService>();
+
+            services.AddHttpClient("UsersApi", client =>
+            {
+                client.BaseAddress = new Uri("http://localhost:5200");
+            });
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Notifications API",
+                    Version = "v1",
+                    Description = "API de gestión de notificaciones del eCommerce."
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+    }
+}
